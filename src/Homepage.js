7498f1a7cdc379b8401ddda7fdf4c1629cc1ef26
FARHA-THASNI-M <farhafaru8979@gmail.com>
2024-12-05 11:32:29 +0530

    changes

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,7 +3,7 @@ import './Homepage.css';
 import image1 from './image1.png';
 import image2 from './image2.png';
 import logo from './logo.png';
-import homeIcon from './Home.png'; // Updated icon file names
+import homeIcon from './Home.png';
 import reelsIcon from './Reel.png';
 import exploreIcon from './Explore.png';
 import profileIcon from './Profile.png';
